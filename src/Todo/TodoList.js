d4c6c672b340a407a96758511a86cf9bcbe72de7
Kettengeschwindigkeit <666damned999@mail.ru>
2023-01-22 10:57:07 +0300

    PropTypes

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import TodoItem from './TodoItem'
 
 const styles = {
@@ -9,10 +10,16 @@ const styles = {
     }
 }
 
-export default function TodoList(props) {
+function TodoList(props) {
     return (
         <ul style={styles.ul}>
             {props.todos.map((todo, index) => <TodoItem todo={todo} key={todo.id} index={index} />)}
         </ul>
     )
 }
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default TodoList
