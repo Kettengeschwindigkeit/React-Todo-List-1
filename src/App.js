4159c1b6704ea82089498a8ab6ee57d82afcbf1f
Kettengeschwindigkeit <666damned999@mail.ru>
2023-01-22 14:58:19 +0300

    React.Suspense (React.lazy)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
-import AddTodo from './Todo/AddTodo'
 import Loader from './Loader'
+
+// const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
+const AddTodo = React.lazy(() => new Promise(resolve => {
+  setTimeout(() => {
+    resolve(import('./Todo/AddTodo'))
+  }, 3000)
+}))
 
 function App() {
   const [todos, setTodos] = React.useState([])
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = React.useState(true)
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
@@ -40,7 +46,9 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className='wrapper'>
         <h1>React tutorial</h1>
-        <AddTodo onCreate={addTodo} />
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
         {loading && <Loader />}
         {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : loading ? null : <p>No todos!</p>}
       </div>
