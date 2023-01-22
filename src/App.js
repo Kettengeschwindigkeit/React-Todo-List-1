7eecf68f7b3f8eeb7d971dd4d777401575a6b8b7
Kettengeschwindigkeit <666damned999@mail.ru>
2023-01-22 14:27:59 +0300

    useEffect()

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: 'To buy bread' },
-    { id: 2, completed: false, title: 'To buy beer' },
-    { id: 3, completed: true, title: 'To buy pot' }
-  ])
+  const [todos, setTodos] = React.useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+        }, 2000)
+      })
+  }, [])
 
   function toggleTodo(id) {
     setTodos(todos.map(todo => {
